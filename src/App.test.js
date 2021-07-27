a08ac5f9f0c5ebabad10daa9fbb8acaf48b66fb9
Gustavo Inocencio <gustavoin.info@gmail.com>
2021-07-27 07:46:23 -0300

    first code quiz

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByRole("link", { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+
+  const but1 = screen.getByRole("button", { name: "Change to blue" });
+
+  expect(but1).toHaveStyle({ backgroundColor: "red" });
+
+  fireEvent.click(but1);
+
+  expect(but1).toHaveStyle({ backgroundColor: "blue" });
+
+  expect(but1.textContent).toBe("Change to red");
+});
+
+test("initial conditions", () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole("checkbox");
+
+  expect(checkbox).not.toBeChecked();
+});
+
+test("button disables on first click and enables on second click", () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  const checkbox = screen.getByRole("checkbox");
+
+  fireEvent.click(checkbox);
+
+  expect(checkbox).toBeChecked();
+
+  expect(colorButton).not.toBeEnabled();
+
+  fireEvent.click(checkbox);
+
+  expect(checkbox).not.toBeChecked();
+
+  expect(colorButton).toBeEnabled();
 });
